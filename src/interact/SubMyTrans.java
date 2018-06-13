60061a49c011e926ea0cc07fbdcdff5f1506b492
HillkingF <976129707@qq.com>
2018-06-13 11:28:09 +0800

    新建分支验证线程  成功

--- src/interact/SubMyTrans.java
+++ src/interact/SubMyTrans.java
@@ -6,7 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-public class SubMyTrans {
+public class SubMyTrans extends Thread {
+
     private String account;
     private String word;
     private String translation;
@@ -19,7 +20,8 @@ public class SubMyTrans {
         System.out.println("在我的类中验证"+account+","+word+","+translation);
     }
 
-    public void submit(){
+    @Override
+    public void run(){
         try {
             //1.获取账号、单词和提交的译文,account word translation 三个字符串在上面定义，是从外部获取到的
 
@@ -57,12 +59,51 @@ public class SubMyTrans {
         }
     }
 
+    /*
+    public void submit(){
+        try {
+            //1.获取账号、单词和提交的译文,account word translation 三个字符串在上面定义，是从外部获取到的
+
+            //2.传入含有中文字符的URL，需要将其进行编码
+            URL url = new URL(Constant.URL_MyTranslation + "account=" + account + "&" +
+                    "word=" + java.net.URLEncoder.encode(word, "utf-8") + "&" +
+                    "translation=" + java.net.URLEncoder.encode(translation, "utf-8") );
+
+            System.out.println(account+"jihao"+word+translation);
+            //接收servlet返回值，是字节
+            InputStream is = url.openStream();
+
+            // 由于is是字节，所以我们要把它转换为String类型，否则遇到中文会出现乱码
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuffer sb = new StringBuffer();
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+
+            // judge result ，页面显示的返回码是否是100（成功）
+            if (sb.toString().equals(Constant.FLAG_SUCCESS)) {
+
+                System.out.println("提交翻译成功！");
+
+            } else if (sb.toString().equals(Constant.FLAG_FAIL)) {
+                //页面显示的返回码是200（失败）
+                System.out.println("提交翻译失败！");
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+*/
 
     //主方法用于测试
 
     public static void main(String[] args){
 
-        new SubMyTrans("111","page","页面").submit();
+        new SubMyTrans("111","page","页面").run();
         //System.out.println(All);
 
     }
